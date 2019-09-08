3ace9b2350471362502b27946bc45e399912a682
a.kapustkin <a.kapustkin@gmail.com>
2019-09-08 11:17:03 +0300

    [task] fix config for rest-server

--- pkg/service/rest-server/config/config.go
+++ pkg/service/rest-server/config/config.go
@@ -17,10 +17,10 @@ type Config struct {
 // InitConfig initial config
 func InitConfig() *Config {
 	cfg := Config{}
-	flag.IntVar(&cfg.Port, "port", 5000, "application port")
-	flag.StringVar(&cfg.Host, "host", "localhost", "application host")
-	flag.IntVar(&cfg.Logging, "log", 0, "application logger. 0 - Disable, 1 - Standart, 2 - Verbose json")
-	flag.StringVar(&cfg.GRPC, "grpc", "localhost:5900", "grpc server application addres")
+	flag.IntVarP(&cfg.Port, "port", "p", 5000, "application port")
+	flag.StringVarP(&cfg.Host, "host", "h", "localhost", "application host")
+	flag.IntVarP(&cfg.Logging, "log", "l", 0, "application logger. 0 - Disable, 1 - Standart, 2 - Verbose json")
+	flag.StringVarP(&cfg.GRPC, "grpc", "g", "localhost:5900", "grpc server application address")
 	flag.Parse()
 	log.Printf("Initital app with config %v", cfg)
 	return &cfg
